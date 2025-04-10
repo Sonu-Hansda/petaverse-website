04fe601c148e1f65c5fcc81ba3fda6dae6d366b6
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-04-10 10:27:31 +0530

    added user data hook

---
+++ src/hooks/useUserData.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { auth, db } from "../../firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+
+const useUserData = () => {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      if (currentUser) {
+        const userDoc = await getDoc(doc(db, "users", currentUser.uid));
+        if (userDoc.exists()) {
+          setUser({ ...userDoc.data(), uid: currentUser.uid });
+        }
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return { user, loading };
+};
+
+export default useUserData;
